6ebd593e37f71aeedc8a190acc4b962318c3665f
Owen Lin <lin.owen@gmail.com>
2022-11-15 15:58:16 -0800

    commit keycontrols

--- Assets/Space Shooter Template FREE/Scripts/PlayerMoving.cs
+++ Assets/Space Shooter Template FREE/Scripts/PlayerMoving.cs
@@ -19,7 +19,8 @@ public class PlayerMoving : MonoBehaviour {
     [Tooltip("offset from viewport borders for player's movement")]
     public Borders borders;
     Camera mainCamera;
-    bool controlIsActive = true; 
+    bool controlIsActive = true;
+
 
     public static PlayerMoving instance; //unique instance of the script for easy access to the script
 
@@ -37,16 +38,22 @@ public class PlayerMoving : MonoBehaviour {
 
     private void Update()
     {
+
+
+
         if (controlIsActive)
         {
 #if UNITY_STANDALONE || UNITY_EDITOR    //if the current platform is not mobile, setting mouse handling 
 
-            if (Input.GetMouseButton(0)) //if mouse button was pressed       
-            {
-                Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition); //calculating mouse position in the worldspace
-                mousePosition.z = transform.position.z;
-                transform.position = Vector3.MoveTowards(transform.position, mousePosition, 30 * Time.deltaTime);
-            }
+            /*  if (Input.GetMouseButton(0)) //if mouse button was pressed       
+              {
+                  Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition); //calculating mouse position in the worldspace
+                  mousePosition.z = transform.position.z;
+                  transform.position = Vector3.MoveTowards(transform.position, mousePosition, 30 * Time.deltaTime);
+              }*/
+            Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            transform.position += movementDirection.normalized * 20 * Time.deltaTime;
+
 #endif
 
 #if UNITY_IOS || UNITY_ANDROID //if current platform is mobile, 
